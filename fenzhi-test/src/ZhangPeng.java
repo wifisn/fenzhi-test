5ba8835e4df7431ae303465583c3bbb9cd04ffd2
JOJO-2918823336 <zhang2918823336gmail.com>
2020-06-03 17:43:29 +0900

    测试

---
+++ fenzhi-test/src/ZhangPeng.java
@@ -0,0 +1,17 @@
+/**
+ * @Package PACKAGE_NAME
+ * @author tom
+ * @date 2020/6/3 17:42
+ * @version V1.0
+ * @Copyright © 株式会社多言语系统研究所
+ */
+
+/**
+ *@ClassName $ {NAME}
+ *@Description TODO
+ *@Author $ {User}
+ *@Date $ {DATE} $ {TIME}
+ *@Version 1.0
+ */
+public class ZhangPeng {
+}
