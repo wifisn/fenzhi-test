6073afe5ce088268d5cf76cecf6eb17c352a6426
syn <s875087178@gmail.com>
2020-06-03 15:17:16 +0900

    添加工程到git

---
+++ gittest/src/test01.java
@@ -0,0 +1,12 @@
+/**
+ * @author 宋永楠
+ * @version v1.0
+ * @Package PACKAGE_NAME
+ * @date 2020/6/3 15:08
+ * @Copyright @ msr
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("11111111");
+    }
+}
