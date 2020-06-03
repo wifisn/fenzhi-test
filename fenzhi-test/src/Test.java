d1539365a0a123b08ec6838f374eb377ffff06c0
syn <s875087178@gmail.com>
2020-06-03 16:42:52 +0900

    创建master

---
+++ fenzhi-test/src/Test.java
@@ -0,0 +1,17 @@
+/**
+ * @author 宋永楠
+ * @version v1.0
+ * @Package PACKAGE_NAME
+ * @date 2020/6/3 16:38
+ * @Copyright @ msr
+ */
+public class Test {
+    public static void main(String[] args) {
+        syn();
+    }
+
+    public static void syn() {
+        System.out.println("宋永楠");
+
+    }
+}
